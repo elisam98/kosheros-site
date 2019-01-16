4750db0835e25a84526afd4aec6f86d5b44a6e92
elisam98 <cornbeefonrye@hotmail.com>
2019-01-16 17:34:28 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home | KosherOS by SafeTelecom | Premium Kosher Smartphone Technology' });
+});
+router.get('/kosher-os', function(req, res, next) {
+  res.render('kosher-os', { title: 'About KosherOS | KosherOS by SafeTelecom | Premium Kosher Smartphone Technology' });
+});
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { title: 'Shop Online | KosherOS by SafeTelecom | Premium Kosher Smartphone Technology' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us | KosherOS by SafeTelecom | Premium Kosher Smartphone Technology' });
+});
+router.get('/app-stores', function(req, res, next) {
+  res.render('app-stores', { title: 'App Stores | KosherOS by SafeTelecom | Premium Kosher Smartphone Technology' });
+});
+router.get('/retail', function(req, res, next) {
+  res.render('retail', { title: 'Store Locator | KosherOS by SafeTelecom | Premium Kosher Smartphone Technology' });
+});
+
+module.exports = router;
