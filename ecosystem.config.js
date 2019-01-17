b0b1c44fc0463fadb7750d813477031c8cc998a6
elisam98 <esamuel@safetelecom.net>
2019-01-17 14:57:29 +0000

    Temp move to query-based shop URLs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,34 +2,7 @@ module.exports = {
   apps : [
     {
       name      : 'KosherOS Site',
-      script    : 'app.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
+      script    : './bin/www'
     }
-  ],
-  deploy : {
-    production : {
-      user : 'node',
-      host : '45.77.201.77',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    },
-    dev : {
-      user : 'node',
-      host : '45.77.201.77',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
-  }
+  ]
 };
